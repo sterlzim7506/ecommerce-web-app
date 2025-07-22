f1b7d6c6126c1f62c1d701b10ca77a4f91232660
sterlker <hazimqayyim1102@gmail.com>
2025-07-22 14:43:41 +0800

    Project update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // routes
 import authRoutes from "./routes/auth.route.js";
